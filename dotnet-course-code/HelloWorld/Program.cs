e684684a8e7bf082727ae8d89ba52fdeb46e29ad
Khoa Nguyen <henrykhoanguyen@gmail.com>
2023-06-17 16:57:44 -0500

    update learning with new materials

---
+++ dotnet-course-code/HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            sbyte mySbyte = 127;
+            byte myByte = 255;
+        }
+    }
+}
